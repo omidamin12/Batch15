98d095757a70c5467d36b2b2982787e26a94dbf0
omidamin12 <fnuomied@gmail.com>
2023-04-01 01:11:12 -0400

    first

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BasicStructure audi =new BasicStructure();
+        audi.color="balck";
+        audi.model="A3";
+        audi.power="5000cc";
+
+        Engine X =new Engine();
+        X.cylinders="8";
+        X.size="1234cc";
+        X.weight="6453k";
+
+
+        color col= new color();
+        col.colorLights="True";
+        col.colorTyre="false";
+        col.colorWindows="True";
+
+        lights neon =new lights();
+        neon.color="red";
+        neon.size="15cm";
+        neon.intensity="7675lumin";
+
+
+    }
+}
